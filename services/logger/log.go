56b1b4ad0b0de3a19e67418b246211cc768a6717
Atoyr <lessironglance@gmail.com>
2020-03-18 19:12:38 +0900

    add logger

---
+++ services/logger/log.go
@@ -0,0 +1,19 @@
+package log
+
+type Logger interface {
+	Infof(format string, v ...interface{})
+	Info(v ...interface{})
+	Infoln(v ...interface{})
+
+	Warnf(format string, v ...interface{})
+	Warn(v ...interface{})
+	Warnln(v ...interface{})
+
+	Errorf(format string, v ...interface{})
+	Error(v ...interface{})
+	Errorln(v ...interface{})
+
+	Debugf(format string, v ...interface{})
+	Debug(v ...interface{})
+	Debugln(v ...interface{})
+}
