4bd860ce987e24288f86c0abc8c3b8609d935bb9
Atoyr <lessironglance@gmail.com>
2020-03-19 15:21:11 +0900

    add Table Define query

---
+++ database/MagicalGirl/MagicalGirl.sql
@@ -0,0 +1,18 @@
+use Magia
+create table MagicalGirl (
+  ID int IDENTITY(1,1) NOT NULL,
+  Name nvarchar(100) NOT NULL,
+  Version nvarchar(50) NOT NULL,
+  RomanName nvarchar(100) NOT NULL,
+  Attribute nvarchar(5) NOT NULL,
+  Type nvarchar(10) NOT NULL,
+  CONSTRAINT PK_MagicalGirl PRIMARY KEY NONCLUSTERED(
+    ID ASC
+  ) WITH (PAD_INDEX = OFF, IGNORE_DUP_KEY=OFF)
+)
+GO
+CREATE CLUSTERED INDEX IDX_RomanName ON MagicalGirl(
+  RomanName ASC
+)WITH (PAD_INDEX = OFF, IGNORE_DUP_KEY=OFF)
+GO
+
