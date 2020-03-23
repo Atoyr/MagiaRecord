ea5406b883e16e012a19561570660f9055d6c166
Atoyr <lessironglance@gmail.com>
2020-03-23 21:43:19 +0900

    add CLI Tools

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/atoyr/MagiaRecord
+
+go 1.12
+
+require github.com/urfave/cli/v2 v2.2.0
--- services/MagicalGirl/main.go
+++ services/MagicalGirl/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/urfave/cli/v2"
+)
+
+const version = "0.0.1"
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "MagiaRecord MagicalGirl Service"
+	app.Usage = "MagiaRecord MagicalGirl Service"
+	app.Version = version
+
+	var httpPort uint
+
+	app.Flags = []cli.Flag{
+		&cli.UintFlag{
+			Name:        "HttpPort",
+			Aliases:     []string{"p"},
+			Value:       8080,
+			EnvVars:     []string{"HTTPPORT"},
+			Destination: &httpPort,
+			Required:    false,
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
