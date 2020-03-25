85aeb9f1816e314ac5114ba6e1003107fc3248ea
Atoyr <lessironglance@gmail.com>
2020-03-25 21:30:01 +0900

    fix spell miss

--- database/Type/Type.sql
+++ database/Type/Type.sql
@@ -2,7 +2,7 @@ create table Type (
   ID int NOT NULL,
   SystemName nvarchar(25),
   Name nvarchar(25),
-  CONSTRATION PK_Type PRIMARY KEY CLUSTERD(
+  CONSTRAINT PK_Type PRIMARY KEY CLUSTERED(
     ID ASC
   ) WITH (PAD_INDEX = OFF, IGNORE_DUP_KEY = OFF)
 )
