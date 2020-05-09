3f51848a2048f692f4fe7b2e7e1dddc7a1d30459
atoyr <lessironglance@gmail.com>
2020-05-10 04:05:26 +0900

    fix pk name

--- database/Attributes/Attributes.sql
+++ database/Attributes/Attributes.sql
@@ -2,7 +2,7 @@ create table Attributes (
   ID int NOT NULL,
   SystemName nvarchar(25),
   Name nvarchar(25),
-  CONSTRAINT PK_Type PRIMARY KEY CLUSTERED(
+  CONSTRAINT PK_Attributes PRIMARY KEY CLUSTERED(
     ID ASC
   ) WITH (PAD_INDEX = OFF, IGNORE_DUP_KEY = OFF)
 );
