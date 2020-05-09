8b0d0004f932796d969cc32e8a44c0d1e1eaef7e
atoyr <lessironglance@gmail.com>
2020-05-10 04:47:12 +0900

    add attribute data

--- database/Attributes/data/v0.0.1.sql
+++ database/Attributes/data/v0.0.1.sql
@@ -1,11 +1,12 @@
 begin tran
 insert into Attributes values(1,'Frame','火')
-insert into Attributes values(1,'Aqua','水')
-insert into Attributes values(1,'Forest','樹')
-insert into Attributes values(1,'light','光')
-insert into Attributes values(1,'Dark','闇')
+insert into Attributes values(2,'Aqua','水')
+insert into Attributes values(3,'Forest','樹')
+insert into Attributes values(4,'Light','光')
+insert into Attributes values(5,'Dark','闇')
+insert into Attributes values(6,'Void','無')
 
-update SystemVersion set Revison = 1 where TableName = 'Attributes'
+update SystemVersion set Mejor = 0, Miner = 0,  Revison = 1 where TableName = 'Attributes';
 
 commit tran
 
